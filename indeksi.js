ac1f6dd313fd0148ecb960b1c051d96e55afbe46
deriel <dennis.holmen@gmail.com>
2022-03-05 21:02:44 +0100

    tog bort brittaniskan

--- indeksi.js
+++ indeksi.js
@@ -25,14 +25,14 @@ dagordningen
 
 dagordningen[kommandorörelse]('hämta')
     [beskrivning]('it fetches the tönts information from the discordz')
-    [handling]((sträng, parametrar) => {
+    [handling](() => {
         inspektörn[när](redo, () => {
             kanalen = inspektörn[kanaler][lagring][hämta]("873614838692192286");
-            kanalen.guild.members.fetch().then(gubbar => {
+            kanalen[gille][medlemmar][inbringa]()[sen](gubbar => {
                 gobbs = {};
 
                 gubbar[förVarje]((gubbe) => {
-                    let jamesCameron = `https://cdn.discordapp.com/avatars/${gubbe.user.id}/${gubbe.user.avatar}`; // troligen viktigt
+                    jamesCameron = `https://cdn.discordapp.com/avatars/${gubbe.user.id}/${gubbe.user.avatar}`; // troligen viktigt
                     gobbs[gubbe.user.id] = {
                         gestalt: gubbe.user.avatar && jamesCameron,
                         nick: gubbe.nickname || gubbe.user.username,
@@ -40,53 +40,57 @@ dagordningen[kommandorörelse]('hämta')
                         gickMed: gubbe.joinedTimestamp,
                     }
                 })
-                äldsta = nycklar(gobbs).reduce((a,y) => Matte.min(a, gobbs[y].gickMed), oändligheten)
-                senaste = nycklar(gobbs).reduce((i,b) => Matte.max(i, gobbs[b].gickMed),0)
+                äldsta = nycklar(gobbs)[förminska]((a,y) => minst(a, gobbs[y].gickMed), oändligheten)
+                senaste = nycklar(gobbs)[förminska]((i,b) => störst(i, gobbs[b].gickMed),0)
                 epok = senaste - äldsta;
-                töntarSomNoder = töntar.filter(t => t.benämning).map(t => {
+                töntarSomNoder = töntar[sila](t => t.benämning)[kartlägg](t => {
                     disco = gobbs[t.discordId]
                     Ålder = disco ? (disco.gickMed - äldsta)/ epok : 1;
                     nod = {
                         id: t.benämning, 
                         [fotnot]: disco?.nick || t.nick || t.benämning,
-                        size: (1-Ålder) * 10 + 25,
+                    [storlek]: (1-Ålder) * 10 + 25,
                     };
                     // notera("hej", t.discordId, !!gobbs[t.discordId], nod[fotnot])
                     om(disco && (nod[fotnot] != disco?.användarnamn), () => nod[fotnot] += "\n"+disco?.användarnamn)   
                     // om(t.typ == "bot", () => nod[form] = "hexagon")
                     om(t.logotyp, () => nod[bild] = t.logotyp, () => om(gobbs[t.discordId], () => nod[bild] = gobbs[t.discordId].gestalt))
-                    om(gobbs[t.discordId], () => delete gobbs[t.discordId])
-                    om(t.storlek, () => nod.size = t.storlek)
+                    om(t.storlek, () => nod[storlek] = t.storlek)
                     return nod;
                 })
-                slutgiltigaNoder = [...töntarSomNoder, ...nycklar(gobbs).map(g => ({
+                gobbs=nycklar(gobbs)[kartlägg](g=>({ g, ...gobbs[g]}))[sila](g => !(töntar[några](t=>(t.discordId==g.g))));
+                slutgiltigaNoder = [...töntarSomNoder, ...gobbs[kartlägg](g => ({
                     id: g,
                     [fotnot]: gobbs[g].nick + (gobbs[g].nick!=gobbs[g].användarnamn?"\n"+gobbs[g].användarnamn:""),
-                    size: (1-(gobbs[g].gickMed-äldsta)/epok)*30+5,
+                [storlek]: (1-(gobbs[g].gickMed-äldsta)/epok)*30+5,
                     [bild]: gobbs[g].gestalt,
                 }))];
-                gobbs=nycklar(gobbs)[kartlägg](g=>({ g, ...gobbs[g]}));
-                gobbs[sortera]((a,b)=>{return a.gickMed>b.gickMed ? 1 : -1});
+                gobbs[sortera]((a,b)=>(a.gickMed>b.gickMed ? 1 : -1));
                 notera(gobbs)
                 notera(äldsta, senaste)
                 notera(`${(gobbs)[längd]} kvar`)
-                relationerSomKanter = relationer.filter(r => r.från && r.till).map((r) => {
+                relationerSomKanter = relationer[sila](r => r.från && r.till)[kartlägg]((r) => {
                     kant = {
                         "from": r.från,
                         "to": r.till,
+                        [streckad]: ["skugga","kodapa","ban"][inkluderar](r.typ),
+                        [pilar]: ["kodapa","ban"][inkluderar](r.typ) ? till : "",
+                        [färg]: förgrena(r.typ, {
+                            'ban': röd,
+                        }),
                     }
-                    förgrena(r.typ, {
-                        'skugga': () => kant[streckad] = sant,
-                        'kodapa': () => {
-                            kant[streckad] = sant
-                            kant[pilar] = till
-                        },
-                        'ban': () => {
-                            kant[streckad] = sant
-                            kant[pilar] = till
-                            kant[färg] = röd
-                        }
-                    })
+                    // förgrena(r.typ, {
+                    //     'skugga': () => kant[streckad] = sant,
+                    //     'kodapa': () => {
+                    //         kant[streckad] = sant
+                    //         kant[pilar] = till
+                    //     },
+                    //     'ban': () => {
+                    //         kant[streckad] = sant
+                    //         kant[pilar] = till
+                    //         kant[färg] = röd
+                    //     }
+                    // })
                     return kant;
                 });
                 filSystem[skrivFilSynkront]('../Lagganstuff/töntz.json', JSON[strängifiera]({ [noder]: slutgiltigaNoder, [kanter]: relationerSomKanter}));
